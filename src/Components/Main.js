05ca0f118db629132296df4247ba840caa63818e
drregg6 <dave.r.regg@gmail.com>
2022-03-05 15:19:19 -0500

    Bare bones react app

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+Main.propTypes = {}
+
+export default Main;
